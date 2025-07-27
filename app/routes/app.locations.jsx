e422746997dcdc1c65833c53107da6a9c38a1ad4
sarim-alis <sarimslayerali786@gmail.com>
2025-07-27 16:50:21 +0500

    feat/Location setup done

--- app/routes/app.locations.jsx
+++ app/routes/app.locations.jsx
@@ -1,24 +1,34 @@
 // Imports.
-import React from 'react';
-import { useLoaderData } from "@remix-run/react";
-import { Page, DataTable, Text, Thumbnail, Badge, BlockStack } from "@shopify/polaris";
+import React, { useState } from 'react';
+import { Page, Text } from "@shopify/polaris";
 import SidebarLayout from "../components/SidebarLayout";
-import { authenticate } from "../shopify.server";
+import { Drawer } from 'antd';
+import { MoreOutlined } from '@ant-design/icons';
+import styles from '../styles/location.js';
 
 
 // Frontend.
 const Locations = () => {
+  const [drawerVisible, setDrawerVisible] = useState(false);
+  const openDrawer = () => setDrawerVisible(true);
+  const closeDrawer = () => setDrawerVisible(false);
+
   return (
     <SidebarLayout>
-     <div style={{ color: "white" }}>
-       <Page fullWidth title="Products">
-            <div style={{ color: "white" }}>
-                <Text variant="headingMd" as="h1">Locations 📍</Text>
-            </div>
-       </Page>
-     </div>
-    </SidebarLayout>
-  )
-}
+      <div style={{ color: "white" }}>
+        <Page fullWidth title="Locations 📍">
+          <div style={styles.container}>
+            <Text variant="headingMd" as="h1">Add Location 🍺</Text>
+            <MoreOutlined onClick={openDrawer} style={{ fontSize: '20px', cursor: 'pointer' }}/>
+          </div>
 
-export default Locations
+          <Drawer title="Add Location 🍺" placement="right" onClose={closeDrawer} open={drawerVisible}>
+            <p>Drawer content like edit/delete settings here…</p>
+          </Drawer>
+        </Page>
+      </div>
+    </SidebarLayout>
+  );
+};
+
+export default Locations;
---
+++ app/styles/location.js
@@ -0,0 +1,13 @@
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: '30rem',
+    marginTop: '1rem',
+    marginBottom: '1rem',
+    color: 'white'
+  },
+};
+
+export default styles;
