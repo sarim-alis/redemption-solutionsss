647c9bac45e7eebf8c219b34567a99518cff8556
sarim-alis <sarimslayerali786@gmail.com>
2025-07-30 11:23:30 +0500

    feat/Login setup done

---
+++ app/styles/login.js
@@ -0,0 +1,45 @@
+const styles = {
+    container: {
+      display: 'flex',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      gap: '8rem',
+      marginTop: '1rem',
+      marginBottom: '1rem',
+      color: 'white',
+      paddingBottom: '20px',
+  },
+    form: {
+      background: '#fff',
+      padding: '30px',
+      borderRadius: '8px',
+      boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+      width: '500px',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    heading: {
+      marginBottom: '20px',
+      textAlign: 'center',
+      color: '#333',
+    },
+    label: {
+      marginBottom: '5px',
+      fontWeight: 'bold',
+      color: '#555',
+    },
+    input: {
+      marginBottom: '15px',
+      padding: '10px',
+      border: '1px solid #ccc',
+      borderRadius: '4px',
+      fontSize: '14px',
+    },
+    button: {
+      backgroundColor: 'rgba(0, 0, 0, 0.45)',
+      borderColor: 'rgba(0, 0, 0, 0.45)',
+      color: 'white',
+  },
+};
+
+export default styles;
