c0653471439d5094c0cfcb8e5f9b275ae954d1c7
sarim-alis <sarimslayerali786@gmail.com>
2025-08-03 00:04:52 +0500

    fears

--- app/routes/app.customers.jsx
+++ app/routes/app.customers.jsx
@@ -25,7 +25,7 @@ const Customers = () => {
      <div style={{ color: "white" }}>
       <Page fullWidth>
         {/* Header */}
-          <Text variant="headingXl" as="h1">Customers 🙍🏻‍♂️⭐🌱</Text>
+        <Text variant="headingXl" as="h1">Customers 🙍🏻‍♂️⭐🌱</Text>
 
         {/* Customer List */}
         <div style={{ marginTop: "40px" }}>
