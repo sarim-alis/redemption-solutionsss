7f75c5fa99d35ca029f102e602c51f76829b5ad2
Hamza92 <hamiking412@gmail.com>
2025-09-08 19:24:45 +0500

    changes

--- app/utils/unifiedEmailTemplate.js
+++ app/utils/unifiedEmailTemplate.js
@@ -31,7 +31,7 @@ function formatCurrency(amount) {
 }
 
 // Generate voucher card HTML
-function generateVoucherCard(voucher) {
+export function generateVoucherCard(voucher) {
   const validThrough = voucher?.createdAt
     ? formatDate(addMonths(voucher.createdAt, 3))
     : "08/16/2026";
@@ -128,7 +128,7 @@ function generateVoucherCard(voucher) {
 }
 
 // Generate gift card HTML - Using original gift card design
-function generateGiftCard(voucher, amount = 0) {
+export function generateGiftCard(voucher, amount = 0) {
   const formattedAmount = formatCurrency(amount);
   
   return `
