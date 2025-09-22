c70a17d615a89692e5814d1de6de3a57b9601062
sarim-alis <sarimslayerali786@gmail.com>
2025-09-22 20:55:55 +0500

    moon-rinngengans

--- app/components/DashboardOrderChart.jsx
+++ app/components/DashboardOrderChart.jsx
@@ -289,14 +289,17 @@ function isDateMatch(dateString, filter, customStart, customEnd) {
             <option key={idx} value={market}>{market}</option>
           ))}
         </select>
-        <select style={styles.select} value={filters.locations} onChange={e => setFilters(f => ({ ...f, locations: e.target.value }))}>
-          <option>All Locations</option>
-          {(filteredLocations || []).map((loc, idx) => (
-            <option key={idx} value={loc.name || loc}>
-              {loc.name || loc}
-            </option>
-          ))}
-        </select>
+        {/* Hide Location filter until Market is selected */}
+        {filters.market !== "All Markets" && (
+          <select style={styles.select} value={filters.locations} onChange={e => setFilters(f => ({ ...f, locations: e.target.value }))}>
+            <option>All Locations</option>
+            {(filteredLocations || []).map((loc, idx) => (
+              <option key={idx} value={loc.name || loc}>
+                {loc.name || loc}
+              </option>
+            ))}
+          </select>
+        )}
         {isFilterActive && <button style={styles.resetButton} onClick={() => { setDateFilter("All"); setCustomStart(""); setCustomEnd(""); setFilters({ products: "All Products", locations: "All Locations" })}}>
           Reset
         </button>}
